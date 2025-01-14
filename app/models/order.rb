0e334c0a1ab43fabfaae6c98f394f75f3da94448
rahuldodwe <rahuldodwe7747909605@gmail.com>
2025-01-14 18:07:18 +0530

    notice pop-up completed

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,6 +3,7 @@ class Order < ApplicationRecord
   has_many :addresses, dependent: :destroy
   has_many :order_items, dependent: :destroy
   has_many :feedbacks
+  has_many :products, through: :order_items
 
   validates :total_price, numericality: { greater_than_or_equal_to: 0 }
 end
